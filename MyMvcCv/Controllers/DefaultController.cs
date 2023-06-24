4ec442e916a0141ba57a32ada7efd6c8fb513ec2
Olya <119108499+Olyala94@users.noreply.github.com>
2023-06-24 08:05:06 +0300

    Add project files.

---
+++ MyMvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using MyMvcCv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();       
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ MyMvcCv/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMvcCv.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test() //"ActionResult" olarak kullanılan mettodlarda <-> geriye bir şey döndürmemiz gerekiyor.
+        {
+            return View();
+        }
+
+        public ActionResult Test2() 
+        {
+            return View();
+        }
+    }
+}
