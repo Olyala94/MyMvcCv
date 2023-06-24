3d7ac57256fc13b857661c30ccd446fcbb8fc521
Olya <119108499+Olyala94@users.noreply.github.com>
2023-06-24 20:20:38 +0300

    5: #24 - Devam

--- MyMvcCv/Repositories/DeneyimRepository.cs
+++ MyMvcCv/Repositories/DeneyimRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using MyMvcCv.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace MyMvcCv.Repositories
 {
-    public class DeneyimRepository
+    public class DeneyimRepository: GenericRepository<TblDeneyimlerim>
     {
 
     }
