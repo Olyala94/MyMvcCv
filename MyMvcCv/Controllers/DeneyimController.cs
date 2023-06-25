d9c2ea2d595aaae08df5b3aae117dfe21f652ac6
Olya <119108499+Olyala94@users.noreply.github.com>
2023-06-25 10:24:02 +0300

    5 : #29 - Db deki Deneyimleri Getirme işlemini yapıldı #30 Getiren Deneyimlerin Detaylarını Görme #31 - Deneyim Güncelleme İşlemi yapıldı

--- MyMvcCv/Controllers/DeneyimController.cs
+++ MyMvcCv/Controllers/DeneyimController.cs
@@ -37,5 +37,23 @@ namespace MyMvcCv.Controllers
             repo.TDelete(t);
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public ActionResult DeneyimGetir(int id)
+        {
+            TblDeneyimlerim t = repo.Find(x=>x.ID == id);   
+            return View(t);
+        }
+        [HttpPost]
+        public ActionResult DeneyimGetir(TblDeneyimlerim p)
+        {
+            TblDeneyimlerim t = repo.Find(x => x.ID == p.ID);
+            t.Baslik = p.Baslik;
+            t.AltBaslik = p.AltBaslik;  
+            t.Tarih = p.Tarih;
+            t.Aciklama = p.Aciklama;
+            repo.TUpdate(t);    
+            return RedirectToAction("Index");
+        }
     }
 }
