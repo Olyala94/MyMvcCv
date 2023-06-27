1abcd73d580d9a0cbb9621ef5406fc883052682f
Olya <119108499+Olyala94@users.noreply.github.com>
2023-06-27 10:01:01 +0300

    Bölüm 8: Sertifika Hobi ve İletişim Sayfaları #46 - Sertifika Sayfa Hazırlığı Yapıldı #47 - Setifika Sayfa HAzırlığı 2 Yapıldı #48 - Sertifika Bilgilerin Listelenmesi

---
+++ MyMvcCv/Controllers/SertifikaController.cs
@@ -0,0 +1,20 @@
+﻿using MyMvcCv.Models.Entity;
+using MyMvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMvcCv.Controllers
+{
+    public class SertifikaController : Controller
+    {
+      GenericRepository<TblSertifikalarim> repo = new GenericRepository<TblSertifikalarim>();
+        public ActionResult Index()
+        {
+            var sertifika = repo.List();
+            return View(sertifika);
+        }
+    }
+}
--- MyMvcCv/Models/Entity/TblSertifikalarim.cs
+++ MyMvcCv/Models/Entity/TblSertifikalarim.cs
@@ -16,5 +16,7 @@ namespace MyMvcCv.Models.Entity
     {
         public int ID { get; set; }
         public string Aciklama { get; set; }
+        public string Tarih { get; set; }
+        public string Baslik { get; set; }
     }
 }
