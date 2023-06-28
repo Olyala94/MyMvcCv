c28b5f60cd73093ebec695711f8151c88e330e20
Olya <119108499+Olyala94@users.noreply.github.com>
2023-06-28 07:42:23 +0300

    Bölüm 8 : Sertifika - #51 - CRUD işlemleri Tamamlandı

--- MyMvcCv/Controllers/SertifikaController.cs
+++ MyMvcCv/Controllers/SertifikaController.cs
@@ -11,10 +11,50 @@ namespace MyMvcCv.Controllers
     public class SertifikaController : Controller
     {
       GenericRepository<TblSertifikalarim> repo = new GenericRepository<TblSertifikalarim>();
+
         public ActionResult Index()
         {
             var sertifika = repo.List();
             return View(sertifika);
+        }
+
+        [HttpGet]
+        public ActionResult SertifikaGetir(int id) 
+        {
+            var sertifika = repo.Find(x => x.ID == id);
+            ViewBag.d = id;
+            return View(sertifika); 
+        }
+
+        [HttpPost]
+        public ActionResult SertifikaGetir(TblSertifikalarim t)
+        {
+            var sertifika = repo.Find(x => x.ID == t.ID);
+            sertifika.Aciklama = t.Aciklama;
+            sertifika.Tarih = t.Tarih;  
+            repo.TUpdate(sertifika);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SertifikaSil(int id)
+        {
+            //var sertifika = repo.Find(x=>x.ID == id);
+            TblSertifikalarim t = repo.Find(x => x.ID == id);
+            repo.TDelete(t);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult YeniSertifika()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniSertifika(TblSertifikalarim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
