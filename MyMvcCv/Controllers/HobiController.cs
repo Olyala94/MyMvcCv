203fba36e83a4d34fa146e75c78996a4a08d4465
Olya <119108499+Olyala94@users.noreply.github.com>
2023-07-01 15:01:40 +0300

    Bölüm 8 :  iletişim ve Hobilerim Sayfaları Yapıldı #ScreenShot resmi eklendi

---
+++ MyMvcCv/Controllers/HobiController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMvcCv.Models.Entity;
+using MyMvcCv.Repositories;
+
+namespace MyMvcCv.Controllers
+{
+    public class HobiController : Controller
+    {
+       GenericRepository<TblHobilerim> repo = new GenericRepository<TblHobilerim>();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var hobiler = repo.List();
+            return View(hobiler);
+        }
+
+        [HttpPost]
+        public ActionResult Index(TblHobilerim p)
+        {
+            // TblHobilerim t = new TblHobilerim();
+            var t = repo.Find(x => x.ID == 1);
+            t.Aciklama1 = p.Aciklama1;  
+            t.Aciklama2 = p.Aciklama2;
+            repo.TUpdate(t);    
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MyMvcCv/Controllers/iletisimController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMvcCv.Models.Entity;
+using MyMvcCv.Repositories;
+
+namespace MyMvcCv.Controllers
+{
+    public class iletisimController : Controller
+    {
+       GenericRepository<Tbliletisim> repo = new GenericRepository<Tbliletisim>();
+
+        public ActionResult Index()
+        {
+            var mesajlar = repo.List();
+            return View(mesajlar);
+        }
+
+        public ActionResult iletisimSil(int id)
+        {
+            Tbliletisim t = repo.Find(x => x.ID == id);
+            repo.TDelete(t);
+            return RedirectToAction("Index");
+        }
+    }
+}
