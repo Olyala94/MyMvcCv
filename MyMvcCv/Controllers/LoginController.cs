0d0ff8702873ea40a55ca7fe017ce730c772eae9
Olya <119108499+Olyala94@users.noreply.github.com>
2023-07-02 21:06:39 +0300

    #65 - Login İşlemleri Yapıldı

--- MyMvcCv/Controllers/LoginController.cs
+++ MyMvcCv/Controllers/LoginController.cs
@@ -1,17 +1,36 @@
-﻿using System;
+﻿using MyMvcCv.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MyMvcCv.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(TblAdmin p)
+        {
+            DbCvEntities db = new DbCvEntities();
+            var bilgi = db.TblAdmin.FirstOrDefault(x => x.KullaniciAdi == p.KullaniciAdi && x.Sifre == p.Sifre);
+           if(bilgi != null) 
+           {
+                FormsAuthentication.SetAuthCookie(bilgi.KullaniciAdi, false);
+                Session["KullaniciAdi"] = bilgi.KullaniciAdi.ToString();
+                return RedirectToAction("Index", "Hakkimda");
+           }
+            else 
+            {
+                return RedirectToAction("Index", "Login");
+            }
         }
     }
 }
